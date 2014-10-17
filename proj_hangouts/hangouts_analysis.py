77eac8e0d3f16991201e345d05880cce4c259668
Skyler <skylergrammer@gmail.com>
2014-10-17 14:36:50 -0500

    some codes in there now

---
+++ proj_hangouts/hangouts_analysis.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import argparse
+import numpy as np
+
+def read_logfile(logfile):
+
+
+
+def main():
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('logfile')
+  args = parser.parse_args()
+  
+
+if __name__ == '__main__':
+  main()
