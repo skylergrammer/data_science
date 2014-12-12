eb787600b359eea1636221191b8e4e796e5cbc43
Skyler Grammer <skylergrammer@gmail.com>
2014-12-12 15:01:27 -0600

    STILL FUCKING BROKEN ON LINE 104

--- skylerdiddles/wine_quality/wine.py
+++ skylerdiddles/wine_quality/wine.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import argparse
 import pandas as pd
 import scipy.stats
+import pybrain.datasets
 
 def read_data(filename):
 
@@ -39,7 +40,7 @@ def make_histograms(data):
 
 
 
-def correlation_matrix(data, target_name=None):
+def correlation_matrix(data, target_name=None, verbose=False):
 
   # Matrix of predictor data
   predictors = [data[x] for x in data if x != target_name]
@@ -47,25 +48,67 @@ def correlation_matrix(data, target_name=None):
   # Compute correlation coefficients matrix
   cor_matrix = np.corrcoef(predictors)
   
-  # Print out matrix; mask out values < 0.7
-  for row in cor_matrix: 
-    row = " ".join(["{:3.2}".format(x) if np.abs(x) > 0.75 else `0.00` for x in row ])
-    print(row)
+  if verbose:
+    # Print out matrix; mask out values < 0.7
+    for row in cor_matrix: 
+      row = " ".join(["{:3.2}".format(x) for x in row ])
+      print(row)
 
   return cor_matrix
 
 
 def zscores(data, target="None"):
 
-  for key in data:
-    if key != target:
-      std = np.nanstd(data[key])
-      mean = np.nanmean(data[key])
-      
-      z = (data[key]-mean) / std
-      data[key] = z
+  # Create a copy of the data frame which will hold the z-scores
+  zdata = pd.DataFrame.copy(data, deep=True)
 
-  return data
+  # Iterate over all columns and replace with z-scores (ignores target column)
+  for key in zdata:
+
+    if key != target:
+      std = np.nanstd(zdata[key])
+      mean = np.nanmean(zdata[key])     
+      z = (zdata[key]-mean) / std
+      
+      zdata[key] = z
+
+  return zdata
+
+
+def construct_NN(data):
+  alldata = pybrain.datasets.ClassificationDataSet(2, 1, nb_classes=3)
+
+
+class NeuralNet:
+
+  def __init__(self, data, target):
+    self.target = data[target]  
+    self.predictors = data.drop(target, axis=1)
+    self.N_predictors = len(self.predictors.columns)
+    self.N_target_vals = len(set(self.target))
+    
+    # Number of data points in target vector
+    N = self.target.shape[0]
+
+    # Validate the data
+    assert N > 0
+    assert self.target.shape == (N,)
+    assert self.predictors.shape == (N, self.N_predictors)
+    assert self.N_target_vals > 1
+    assert self.N_predictors > 0
+
+
+  def construct(self):
+    # Contsuct dataset structure for NN
+    alldata = pybrain.datasets.ClassificationDataSet(self.N_predictors, 1, nb_classes=self.N_target_vals)
+    alldata.addSample(self.predictors, self.target)
+
+    # Construct the training and testing data sets
+    tstdata, trndata = alldata.splitWithProportion(0.25)
+    
+    # Initialize training and testing data sets
+    trndata._convertToOneOfMany()
+    tstdata._convertToOneOfMany()
 
 
 def main():
@@ -74,14 +117,22 @@ def main():
   parser.add_argument("input")
   args = parser.parse_args()
 
+  # Identify target column
   target = "quality"
 
+  # Readx data into a pandas data frame
   data = read_data(args.input)
 
-  cor_matrix = correlation_matrix(data)  
+  # Compute correlation matrix
+  cor_matrix = correlation_matrix(data, verbose=False)  
 
+  # Transform data to z-scores (mean=0, std=1)
   zdata = zscores(data, target=target)
 
+
+  NNdata = NeuralNet(zdata, target)
+
+  NNdata.construct()
 
 if __name__ == "__main__":
   main()
