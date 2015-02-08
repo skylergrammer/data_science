4812050f38ae3ec48ebaa9eb1807fa77964854c9
Skyler Grammer <sgrammer@macbook-sky.local>
2015-02-08 11:17:24 -0600

    mostly works

--- random_word_cloud/make_word_cloud.py
+++ random_word_cloud/make_word_cloud.py
@@ -1,7 +1,7 @@
 #! /usr/bin/evn python
 
 import bs4
-import urllib
+import requests
 import argparse
 import pytagcloud as ptc
 from pytagcloud.lang.counter import get_tag_counts
@@ -9,49 +9,64 @@ import webbrowser
 
 def get_raw_text(url):
 
-    html = urllib.urlopen(url).read().decode("utf8")
+    r = requests.get(url)
     
-    soup = bs4.BeautifulSoup(html, "html.parser").prettify()
+    soup = bs4.BeautifulSoup(r.content).findAll("p")
     
-    print(soup.find_all("entry-content"))
+    raw_text = []
+    for paragraph in soup: 
+        for section in paragraph.findAll(text=True): 
+            good = " ".join([x.lower() for x in section.split() if x.isalnum()])
+            raw_text.append(good)
+            
+    raw_text = " ".join(raw_text)
     
-    return raw
+    return raw_text
 
+    
+def get_except_words(filename):
+    
+    with open(filename, "r") as f:
+        file_content = f.readlines()
+        
+        words = [x.strip("\n") for x in file_content]  
+
+    return words
+    
     
 class TextConstruct:
 
-    def __init__(self, raw_text, url):        
-    
-        bad_words = ["rett", "research", "syndrome"]
-    
-        url_words = url.replace("/",".").replace("-",".").split(".")
-        bad_words = bad_words + url_words
+    def __init__(self, raw_text, url, except_words):        
         
-        filtered = " ".join([x.lower() for x in raw_text.split() 
-            if len(x) > 3 and x.isalpha() and x.lower() not in bad_words])
-            
-        self.filtered = filtered
+        url_words = url.replace("/",".").replace("-",".").split(".")    
+        except_words = except_words + url_words
+        
+        filtered = " ".join([x for x in raw_text.split() 
+                             if len(x) > 2 and x not in except_words])
+                             
         tag_counts = get_tag_counts(filtered)
+        
+        self.filtered = filtered
         self.tags = ptc.make_tags(tag_counts, maxsize=100)
+        self.filename = "".join([x for x in url_words if x not in ["http:","www"]])+".png"
 
     def generate_cloud(self):
     
-        ptc.create_tag_image(self.tags, "test.png", size=(1024, 1024), fontname="Droid Sans")
+        ptc.create_tag_image(self.tags, self.filename, size=(1024, 1024), fontname="Droid Sans")
                    
 def main():
 
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--url")
+    parser.add_argument("--e", dest="except_words")
     args = parser.parse_args()
     
     raw = get_raw_text(args.url)
+    except_words = get_except_words(args.except_words)
 
-    '''
-    foo = TextConstruct(raw, args.url)
+    foo = TextConstruct(raw, args.url, except_words)
 
     foo.generate_cloud() 
-    '''   
         
 if __name__ == "__main__":
     main()
